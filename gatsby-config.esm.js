250743bbdbaacf2a4c65dd300ecc190e883e206b
Jay Lee <jlee@urbaninsight.com>
2022-07-14 16:27:28 -0700

    fix errors

---
+++ gatsby-config.esm.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+export default {
+  siteMetadata: {
+    title: `Slicks Slices`,
+    siteUrl: 'https://gatsby.pizza',
+    description: 'The best pizza place in Hamilton!',
+    twitter: '@slickSlices',
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // this is the name of the plugin you are adding
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'rc6g6wyk',
+        dataset: 'live',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+    'gatsby-plugin-react-helmet',
+  ],
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,2 @@
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '.env' });
-
-export default {
-  siteMetadata: {
-    title: `Slicks Slices`,
-    siteUrl: 'https://gatsby.pizza',
-    description: 'The best pizza place in Hamilton!',
-    twitter: '@slickSlices',
-  },
-  plugins: [
-    'gatsby-plugin-styled-components',
-    {
-      // this is the name of the plugin you are adding
-      resolve: 'gatsby-source-sanity',
-      options: {
-        projectId: 'rc6g6wyk',
-        dataset: 'live',
-        watchMode: true,
-        token: process.env.SANITY_TOKEN,
-      },
-    },
-    'gatsby-plugin-react-helmet',
-  ],
-};
+require = require('esm')(module)
+module.exports = require('./gatsby-config.esm.js')
